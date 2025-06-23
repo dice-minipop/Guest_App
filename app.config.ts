18a9c3c97879a7cd22eaa3287176e47777128dda
JJHyeok13 <jjinh99@naver.com>
2025-06-23 15:05:24 +0900

    chore: 라이브러리 및 gitignore 추가, config 설정

--- app.config.ts
+++ app.config.ts
@@ -5,13 +5,12 @@ config();
 
 const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 const projectId = process.env.EXPO_PUBLIC_PROJECT_ID;
-const apiKey = process.env.EXPO_PUBLIC_GOOGLEMAP_API_KEY;
 
 const defineConfig = (_: ConfigContext): ExpoConfig => ({
   owner: 'minipop',
   name: 'dice - 팝업 운영 올인원 솔루션',
   slug: 'dice',
-  version: '1.0.0',
+  version: '1.1',
   orientation: 'portrait',
   icon: './assets/images/icon.png',
   scheme: 'myapp',
@@ -29,21 +28,24 @@ const defineConfig = (_: ConfigContext): ExpoConfig => ({
   updates: {
     url: `https://u.expo.dev/${projectId}`,
   },
-  runtimeVersion: '1.0.0',
+  runtimeVersion: '1.1',
   ios: {
     supportsTablet: true,
     bundleIdentifier: 'com.cmc.dice.minipop.expo',
-    buildNumber: '1.0.1',
+    buildNumber: '1.0.0',
+    entitlements: {
+      'aps-environment': 'production',
+    },
     infoPlist: {
+      NSUserNotificationUsageDescription: '푸시 알림을 통해 중요한 알림을 받을 수 있습니다.',
       NSPhotoLibraryUsageDescription:
         'The app accesses the photo library to upload your profile when you are signing up or updating profile image.',
       NSAppTransportSecurity: {
         NSAllowsArbitraryLoads: true,
       },
+      UIBackgroundModes: ['remote-notification'],
     },
-    config: {
-      googleMapsApiKey: apiKey,
-    },
+    googleServicesFile: process.env.GOOGLE_SERVICES_INFO_PLIST ?? './GoogleService-Info.plist',
   },
   android: {
     package: 'com.cmc.dice.minipop.expo',
@@ -53,11 +55,7 @@ const defineConfig = (_: ConfigContext): ExpoConfig => ({
       backgroundColor: '#000000',
     },
     permissions: [],
-    config: {
-      googleMaps: {
-        apiKey: apiKey,
-      },
-    },
+    googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './google-services.json',
   },
   web: {
     bundler: 'metro',
@@ -66,19 +64,15 @@ const defineConfig = (_: ConfigContext): ExpoConfig => ({
   },
   plugins: [
     'expo-router',
+    '@react-native-firebase/app',
+    '@react-native-firebase/messaging',
     [
       'expo-splash-screen',
       {
         image: './assets/images/splash.png',
         imageWidth: 97,
         resizeMode: 'contain',
-        backgroundColor: '#ffffff',
-      },
-    ],
-    [
-      '@mj-studio/react-native-naver-map',
-      {
-        client_id: 'pk2penm8t7',
+        backgroundColor: '#000000',
       },
     ],
     [
@@ -87,6 +81,9 @@ const defineConfig = (_: ConfigContext): ExpoConfig => ({
         android: {
           extraMavenRepos: ['https://repository.map.naver.com/archive/maven'],
           enableProguardInReleaseBuilds: true,
+        },
+        ios: {
+          useFrameworks: 'static',
         },
       },
     ],
