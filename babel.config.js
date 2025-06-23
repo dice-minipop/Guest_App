3439fb3fb9dd27a46a85756c6ea966cc952bbe84
JJHyeok13 <jjinh99@naver.com>
2025-06-23 15:31:18 +0900

    refactor: 추가 리팩토링

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-    plugins: [
-      // Reanimated plugin must be listed last
-      'react-native-reanimated/plugin',
-      // 'react-native-worklets/plugin',
-    ],
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
