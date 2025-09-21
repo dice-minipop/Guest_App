d6db739d418db22bea9df509b5944dcee12013ef
JJHyeok13 <jjinh99@naver.com>
2025-09-21 16:04:04 +0900

    fix: 회원가입 스크린 수정

--- app/(onBoarding)/register.tsx
+++ app/(onBoarding)/register.tsx
@@ -1,87 +1,86 @@
-import { router } from 'expo-router';
+import { useRouter } from 'expo-router';
 import { useForm } from 'react-hook-form';
-import { Pressable, Text, TouchableWithoutFeedback } from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import XIcon from '@/assets/icons/x.svg';
-import CustomPressableComponent from '@/components/common/customPressable';
-import InputControllerComponent from '@/components/onBoard/inputController';
+import BackHeaderComponent from '@/components/common/backHeader';
+import CTAContainer from '@/components/common/ctaContainer';
+import CustomPressable from '@/components/common/customPressable/customPressable';
+import RegisterForm from '@/components/onBoard/register/registerForm';
 import { useSignUp } from '@/hooks/auth/auth';
-import { useDomainModalStore } from '@/zustands/onBoard/store';
-
-type FormData = {
-  name: string;
-  email: string;
-  password: string;
-  passwordCheck: string;
-  phone: string;
-};
+import KeyBoardAwareProvider from '@/providers/keyBoardProvider';
+import { RegisterDto } from '@/types/auth';
+import {
+  emailValidate,
+  passwordCheckValidate,
+  passwordValidate,
+  // phoneValidate,
+} from '@/utils/validators';
 
 export default function Register() {
-  const formFields: { title: string; name: keyof FormData; placeholder: string }[] = [
+  const router = useRouter();
+
+  const formFields: {
+    title: string;
+    name: keyof RegisterDto;
+    placeholder: string;
+    validate?: (value: string, formValues: RegisterDto) => true | string | Promise<true | string>;
+  }[] = [
     { title: '이름', name: 'name', placeholder: '이름을 입력해주세요' },
-    { title: '이메일 아이디', name: 'email', placeholder: '예: dice16' },
-    { title: '비밀번호', name: 'password', placeholder: '비밀번호를 입력해주세요' },
+    {
+      title: '이메일 아이디',
+      name: 'email',
+      placeholder: '이메일을 입력해주세요',
+      validate: emailValidate,
+    },
+    {
+      title: '비밀번호',
+      name: 'password',
+      placeholder: '비밀번호를 입력해주세요',
+      validate: passwordValidate,
+    },
     {
       title: '비밀번호 확인',
       name: 'passwordCheck',
       placeholder: '비밀번호를 한번 더 입력해주세요',
+      validate: (value, password) => passwordCheckValidate(value, password!),
     },
-    { title: '휴대폰', name: 'phone', placeholder: '숫자만 입력해주세요' },
+    // { title: '휴대폰', name: 'phone', placeholder: '숫자만 입력해주세요', validate: phoneValidate },
   ];
 
   const {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<RegisterDto>({ mode: 'onChange' });
 
   const { mutateAsync: signUp } = useSignUp();
 
-  const onSubmit = async (data: FormData) => {
+  const onSubmit = async (data: RegisterDto) => {
     await signUp({
       email: data.email,
       name: data.name,
       password: data.password,
-      phone: data.phone,
+      // phone: data.phone,
       userRole: 1,
     });
   };
 
-  const { setIsDomainModalOpen } = useDomainModalStore();
-
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <Pressable onPress={() => router.back()} className="self-start p-[12px] ml-[3px]">
-        <XIcon />
-      </Pressable>
+    <SafeAreaView className="flex-1 bg-white relative">
+      <BackHeaderComponent style="WHITE" hasSafeArea={false} title="회원가입" />
 
-      <TouchableWithoutFeedback onPress={() => setIsDomainModalOpen(false)}>
-        <KeyboardAwareScrollView
-          contentContainerStyle={{ paddingHorizontal: 20, rowGap: 24, paddingBottom: 64 }}
-          keyboardShouldPersistTaps="handled"
-          enableOnAndroid={true}
-          extraScrollHeight={20}
-        >
-          <Text className="H1 text-black mt-[32px] mb-[8px]">회원가입</Text>
-          {formFields.map((field) => (
-            <InputControllerComponent
-              key={field.name}
-              control={control}
-              title={field.title}
-              name={field.name}
-              placeholder={field.placeholder}
-            />
-          ))}
-        </KeyboardAwareScrollView>
-      </TouchableWithoutFeedback>
+      <KeyBoardAwareProvider rowGap={11}>
+        <RegisterForm formFields={formFields} control={control} errors={errors} />
+      </KeyBoardAwareProvider>
 
-      <CustomPressableComponent
-        buttonText="회원가입"
-        onPress={handleSubmit(onSubmit)}
-        disabled={false}
-      />
+      <CTAContainer extraBottom={16}>
+        <CustomPressable
+          buttonText="다음"
+          // onPress={handleSubmit(onSubmit)}
+          onPress={() => router.push('/(onBoarding)/brandProfile')}
+          disabled={false}
+        />
+      </CTAContainer>
     </SafeAreaView>
   );
 }
