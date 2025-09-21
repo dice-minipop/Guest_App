b90a95425cc697995d315feb08310ef5389d6814
JJHyeok13 <jjinh99@naver.com>
2025-09-21 18:54:57 +0900

    fix: 공간 상세 스크린 하단 버튼 컨테이너 컴포넌트 수정

--- components/space/detail/bottomButtonContainer.tsx
+++ components/space/detail/bottomButtonContainer.tsx
@@ -1,32 +1,47 @@
-import { BottomSheetMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
-import { Text, TouchableOpacity, View } from 'react-native';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { Fragment, RefObject, useRef } from 'react';
+import { Text, View } from 'react-native';
 
 import ChatIcon from '@/assets/icons/chat.svg';
-import PhoneIcon from '@/assets/icons/phone.svg';
 import ReservationIcon from '@/assets/icons/reservation.svg';
+import OpacityPressable from '@/components/common/opacityPressable';
+import { SpaceDetailItem } from '@/types/space';
+
+import ReservationModalComponent from './reservationModal';
 
 interface BottomButtonContainerProps {
-  bottomSheetRef: React.RefObject<BottomSheetMethods>;
+  spaceId: number;
+  data?: SpaceDetailItem;
 }
 
-const BottomButtonContainer: React.FC<BottomButtonContainerProps> = ({ bottomSheetRef }) => {
-  return (
-    <View className="flex flex-row items-center gap-x-[8px] px-[20px] pt-[16px] pb-[50px] border-t border-t-stroke">
-      <TouchableOpacity className="p-[14px] border border-light_gray rounded-lg">
-        <PhoneIcon />
-      </TouchableOpacity>
-      <TouchableOpacity className="px-[14.5px] py-[14px] border border-light_gray rounded-lg">
-        <ChatIcon />
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={() => bottomSheetRef.current?.expand()}
-        className="flex-1 flex flex-row justify-center items-center gap-x-[8px] bg-black rounded-lg py-[15px]"
-      >
-        <ReservationIcon />
-        <Text className="BTN1 text-white">공간 예약하기</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+export default function BottomButtonContainer({ spaceId, data }: BottomButtonContainerProps) {
+  const bottomSheetRef = useRef<BottomSheet>(null);
 
-export default BottomButtonContainer;
+  return (
+    <Fragment>
+      <View className="bg-white border-t border-t-stroke">
+        <View className="flex flex-row gap-x-[8px] px-[20px] pt-[16px] pb-[50px]">
+          <OpacityPressable className="px-[14.5px] py-[14px] border border-light_gray rounded-lg">
+            <ChatIcon />
+          </OpacityPressable>
+
+          <OpacityPressable
+            onPress={() => bottomSheetRef.current?.expand()}
+            className="flex-1 flex flex-row justify-center items-center gap-x-[8px] bg-black rounded-lg py-[15px]"
+          >
+            <ReservationIcon />
+            <Text className="BTN1 text-white">공간 예약 신청</Text>
+          </OpacityPressable>
+        </View>
+      </View>
+
+      {data !== undefined && (
+        <ReservationModalComponent
+          spaceId={spaceId}
+          data={data}
+          bottomSheetRef={bottomSheetRef as RefObject<BottomSheet>}
+        />
+      )}
+    </Fragment>
+  );
+}
