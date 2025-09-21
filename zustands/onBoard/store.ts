755acace76cf8cf7b2c1a9286277531fcab01bc7
JJHyeok13 <jjinh99@naver.com>
2025-09-21 16:05:43 +0900

    feat: 회원가입용 zustand store 정의

--- zustands/onBoard/store.ts
+++ zustands/onBoard/store.ts
@@ -1,5 +1,88 @@
 import { create } from 'zustand';
 
+import { SignUpStore } from './type';
+
+export const useSignUpStore = create<{
+  signUpStore: SignUpStore;
+  setSignUpStore: (state: Partial<SignUpStore>) => void;
+
+  setTargetGender: (genders: string[]) => void;
+  setTargetAgeGroup: (ages: number[]) => void;
+  setBrandName: (name: string) => void;
+  setBrandDescription: (desc: string) => void;
+  setLogoUrl: (url: string) => void;
+  setImageUrls: (urls: string[]) => void;
+}>((set) => ({
+  signUpStore: {
+    signUp: {
+      email: '',
+      name: '',
+      password: '',
+      phone: '',
+      userRole: 1,
+    },
+    brandProfile: {
+      targetGender: [],
+      targetAgeGroup: [],
+      name: '',
+      description: '',
+      logoUrl: '',
+      imageUrls: [],
+    },
+  },
+  setSignUpStore: (newState) =>
+    set((state) => ({
+      signUpStore: {
+        ...state.signUpStore,
+        ...newState,
+      },
+    })),
+
+  // 브랜드 프로필 관련 setters
+  setTargetGender: (targetGender: string[]) =>
+    set((state) => ({
+      signUpStore: {
+        ...state.signUpStore,
+        brandProfile: { ...state.signUpStore.brandProfile, targetGender },
+      },
+    })),
+  setTargetAgeGroup: (targetAgeGroup: number[]) =>
+    set((state) => ({
+      signUpStore: {
+        ...state.signUpStore,
+        brandProfile: { ...state.signUpStore.brandProfile, targetAgeGroup },
+      },
+    })),
+  setBrandName: (name: string) =>
+    set((state) => ({
+      signUpStore: {
+        ...state.signUpStore,
+        brandProfile: { ...state.signUpStore.brandProfile, name },
+      },
+    })),
+  setBrandDescription: (description: string) =>
+    set((state) => ({
+      signUpStore: {
+        ...state.signUpStore,
+        brandProfile: { ...state.signUpStore.brandProfile, description },
+      },
+    })),
+  setLogoUrl: (logoUrl: string) =>
+    set((state) => ({
+      signUpStore: {
+        ...state.signUpStore,
+        brandProfile: { ...state.signUpStore.brandProfile, logoUrl },
+      },
+    })),
+  setImageUrls: (imageUrls: string[]) =>
+    set((state) => ({
+      signUpStore: {
+        ...state.signUpStore,
+        brandProfile: { ...state.signUpStore.brandProfile, imageUrls },
+      },
+    })),
+}));
+
 // 이메일 도메인 선택 모달 store
 export const useDomainModalStore = create<{
   isDomainModalOpen: boolean;
