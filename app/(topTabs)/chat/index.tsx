f419fc6fe9d4e015c32a6227a7f2201ebe5ac5c9
JJHyeok13 <jjinh99@naver.com>
2025-09-21 20:07:26 +0900

    refactor: 쪽지 목록 스크린 리팩토링 및 Skeleton UI 적용

--- app/(topTabs)/chat/index.tsx
+++ app/(topTabs)/chat/index.tsx
@@ -1,37 +1,48 @@
-import { FlatList, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import ContentLoader, { Rect } from 'react-content-loader/native';
+import { Dimensions, FlatList, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-import ChatIcon from '@/assets/icons/chat/chat.svg';
 import ChatListHeaderComponent from '@/components/chat/chatListHeader';
 import ChatRoomItemComponent from '@/components/chat/chatRoomItem';
 import BackHeaderComponent from '@/components/common/backHeader';
 import { useGetMessageLists } from '@/hooks/message/message';
 
 export default function ChatList() {
-  // const { data } = useGetMessageLists();
+  const { data, isLoading, isError } = useGetMessageLists();
+  const chatRoomData = data || [];
 
-  const data = [
-    {
-      id: 1,
-      spaceName: '이름이름이름이름이름이름이름이름이름이름이름이름',
-      spaceImage: 'https://picsum.photos/250/250',
-      lastMessage:
-        '안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요',
-      lastMessageAt: '오전 10시',
-      unreadCount: 10,
-    },
-  ];
+  if (isError) {
+    return (
+      <SafeAreaView className="flex-1 bg-white">
+        <StatusBar style="dark" />
+        <BackHeaderComponent style="WHITE" hasSafeArea={false} />
+
+        <View className="flex-1 justify-center items-center">
+          <Text className="BODY1 text-red">잘못된 요청입니다!</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
 
   return (
     <SafeAreaView className="flex-1 bg-white">
+      <StatusBar style="dark" />
+
       <BackHeaderComponent style="WHITE" hasSafeArea={false} />
 
       <FlatList
-        contentContainerStyle={{ flex: 1, paddingBottom: 64 }}
-        data={data}
+        contentContainerStyle={{ flexGrow: 1, paddingBottom: 64 }}
+        data={isLoading ? Array.from({ length: 3 }).map(() => null) : chatRoomData}
         ListHeaderComponent={() => <ChatListHeaderComponent />}
         stickyHeaderIndices={[0]}
-        renderItem={({ item }) => <ChatRoomItemComponent key={item.id} data={item} />}
+        renderItem={({ item, index }) =>
+          item ? (
+            <ChatRoomItemComponent key={item.id} data={item} />
+          ) : (
+            <ChatRoomSkeleton key={index} />
+          )
+        }
         ItemSeparatorComponent={() => <View className="h-[12px]" />}
         ListEmptyComponent={() => (
           <View className="flex-1 flex justify-center items-center">
@@ -42,3 +53,23 @@ export default function ChatList() {
     </SafeAreaView>
   );
 }
+
+const ChatRoomSkeleton = () => {
+  return (
+    <ContentLoader
+      speed={2}
+      width={Dimensions.get('screen').width - 40}
+      height={80}
+      backgroundColor="#ecebeb"
+      foregroundColor="#f3f3f3"
+    >
+      {/* 주소 */}
+      <Rect x="20" y="15" rx="8" ry="8" width="50" height="50" />
+
+      {/* 이름 */}
+      <Rect x="82" y="15" rx="4" ry="4" width="200" height="27" />
+      {/* 사이즈 */}
+      <Rect x="82" y="44" rx="4" ry="4" width="200" height="22" />
+    </ContentLoader>
+  );
+};
