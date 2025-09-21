ed5bddcc71ce04f42504d098e3c74ed2afcca1fe
JJHyeok13 <jjinh99@naver.com>
2025-09-21 15:59:30 +0900

    feat: 텍스트 형태의 공통 버튼 컴포넌트 구현

---
+++ components/common/textButton.tsx
@@ -0,0 +1,22 @@
+import { ReactNode, useState } from 'react';
+import { Pressable, Text } from 'react-native';
+
+interface TextButtonProps {
+  children: ReactNode;
+  onPress: () => void;
+}
+
+export default function TextButton({ children, onPress }: TextButtonProps) {
+  const [isPressed, setIsPressed] = useState<boolean>(false);
+
+  return (
+    <Pressable
+      onPress={onPress}
+      onPressIn={() => setIsPressed(true)}
+      onPressOut={() => setIsPressed(false)}
+      className={`self-center px-[16px] py-[13.5px] ${isPressed && 'opacity-60'}`}
+    >
+      <Text className="BTN1 text-medium_gray text-center underline">{children}</Text>
+    </Pressable>
+  );
+}
