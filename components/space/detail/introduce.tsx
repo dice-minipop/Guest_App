ff8b9c722355efef9ce4e7601111b79ce0e3ea8c
JJHyeok13 <jjinh99@naver.com>
2025-09-21 18:52:35 +0900

    fix: 공간 소개 정보 컴포넌트 수정

--- components/space/detail/introduce.tsx
+++ components/space/detail/introduce.tsx
@@ -3,29 +3,31 @@ import { Text, View } from 'react-native';
 
 import { SpaceDetailComponentProps } from '@/types/space';
 
-import CustomPressableComponent from '../../common/customPressable';
+import CustomPressable from '../../common/customPressable/customPressable';
 
-const SpaceIntroduceComponent: React.FC<SpaceDetailComponentProps> = ({ data }) => {
+export default function SpaceIntroduceComponent({ data }: SpaceDetailComponentProps) {
   const [isSeeMore, setIsSeeMore] = useState<boolean>(false);
 
   return (
-    <View className="gap-y-[16px]">
+    <View>
       <View className="gap-y-[16px] px-[20px]">
-        <Text className="SUB2 text-black">팝업 공간 소개</Text>
-        <Text numberOfLines={isSeeMore ? undefined : 3} className="BODY1 text-deep_gray">
-          {data.details}
-        </Text>
+        <View className="gap-y-[16px]">
+          <Text className="SUB2 text-black">팝업 공간 소개</Text>
+          <Text numberOfLines={isSeeMore ? undefined : 3} className="BODY1 text-deep_gray">
+            {data.details}
+          </Text>
+        </View>
+
+        <CustomPressable
+          buttonText={isSeeMore ? '간략히 보기' : '자세히 보기'}
+          onPress={() => setIsSeeMore(!isSeeMore)}
+          disabled={false}
+          color="WHITE"
+          arrow={isSeeMore ? 'UP' : 'DOWN'}
+        />
       </View>
 
-      <CustomPressableComponent
-        buttonText={isSeeMore ? '간략히 보기' : '자세히 보기'}
-        onPress={() => setIsSeeMore(!isSeeMore)}
-        disabled={false}
-        color="WHITE"
-        arrow={isSeeMore ? 'UP' : 'DOWN'}
-      />
+      <View className="h-[8px] bg-back_gray my-[24px]" />
     </View>
   );
-};
-
-export default SpaceIntroduceComponent;
+}
