ebaabe8eb0f427a1741cdbb9b0f8b2c5b6e5002d
JJHyeok13 <jjinh99@naver.com>
2025-09-21 19:00:13 +0900

    feat: 공간 예약 행사 정보 입력 스크린 구현

--- zustands/reservation/store.ts
+++ zustands/reservation/store.ts
@@ -13,3 +13,41 @@ export const useReservationStore = create<{
   },
   setReservationData: (status) => set({ reservationData: status }),
 }));
+
+interface CreateReservation {
+  spaceId: number;
+  startDate: string;
+  endDate: string;
+  eventName: string;
+  eventContent: string;
+  fileList: string[];
+  etcRequest: string;
+}
+
+interface CreateReservationStore {
+  reservationData: CreateReservation;
+  setReservationData: <K extends keyof CreateReservation>(
+    key: K,
+    value: CreateReservation[K],
+  ) => void;
+}
+
+export const useCreateReservationStore = create<CreateReservationStore>((set) => ({
+  reservationData: {
+    spaceId: 0,
+    startDate: '',
+    endDate: '',
+    eventName: '',
+    eventContent: '',
+    fileList: [],
+    etcRequest: '',
+  },
+
+  setReservationData: (key, value) =>
+    set((state) => ({
+      reservationData: {
+        ...state.reservationData,
+        [key]: value,
+      },
+    })),
+}));
