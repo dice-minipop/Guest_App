3c766d345864a2ebb58ac65f8be5816338301f0e
JJHyeok13 <jjinh99@naver.com>
2025-09-21 20:03:50 +0900

    refactor: 공고 상세 스크린 컴포넌트 구분 및 리팩토링

--- app/announcement/[id].tsx
+++ app/announcement/[id].tsx
@@ -2,10 +2,11 @@ import { useLocalSearchParams } from 'expo-router';
 import { ScrollView, View } from 'react-native';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import AnnouncementBasicInfoComponent from '@/components/announcement/detail/basicInfo';
-import AnnouncementImageListComponent from '@/components/announcement/detail/imageList';
-import AnnouncementIntroduceComponent from '@/components/announcement/detail/introduce';
+import AnnouncementBasicInfo from '@/components/announcement/detail/basicInfo';
+import AnnouncementImageList from '@/components/announcement/detail/imageList';
+import AnnouncementIntroduce from '@/components/announcement/detail/introduce';
 import BackHeaderComponent from '@/components/common/backHeader';
+import { dummyData } from '@/constants/dummyData/announcementDetail';
 import { useGetAnnouncementDetailData } from '@/hooks/announcement/announcement';
 
 export default function AnnouncementDetail() {
@@ -14,36 +15,17 @@ export default function AnnouncementDetail() {
   const { bottom } = useSafeAreaInsets();
 
   // const { data } = useGetAnnouncementDetailData(Number(id));
-
-  const data = {
-    id: 1,
-    title: '이름',
-    city: '서울',
-    district: '중랑구',
-    address: '머시기로',
-    hostName: '호스트네임',
-    target: '소상공인',
-    imageUrls: ['https://picsum.photos/250/250'],
-    recruitmentStartAt: '시작시간',
-    recruitmentEndAt: '끝시간',
-    details:
-      '소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개소개',
-    contactNumber: '010-1234-5678',
-    websiteUrl: 'https://google.co.kr',
-    isLiked: false,
-    likeCount: 123,
-    status: '상태',
-  };
+  const data = dummyData;
 
   return (
     <View className="flex-1 bg-white">
       <BackHeaderComponent />
 
       <ScrollView contentContainerStyle={{ paddingBottom: 160 }}>
-        <AnnouncementImageListComponent data={data} />
-        <AnnouncementBasicInfoComponent data={data} />
+        <AnnouncementImageList data={data} />
+        <AnnouncementBasicInfo data={data} />
         <View className="h-[8px] bg-back_gray my-[24px]" />
-        <AnnouncementIntroduceComponent data={data} />
+        <AnnouncementIntroduce data={data} />
       </ScrollView>
 
       <SafeAreaView edges={['bottom']} style={{ height: bottom, backgroundColor: '#FFFFFF' }} />
