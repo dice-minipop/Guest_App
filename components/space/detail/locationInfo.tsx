bb5b851f7e1c87fa64017c487a575b80c05f45cc
JJHyeok13 <jjinh99@naver.com>
2025-09-21 18:53:19 +0900

    fix: 공간 위치 안내 정보 컴포넌트 수정

--- components/space/detail/locationInfo.tsx
+++ components/space/detail/locationInfo.tsx
@@ -5,54 +5,57 @@ import { Alert, Pressable, Text, View } from 'react-native';
 import WebView from 'react-native-webview';
 
 import MarkerIcon from '@/assets/icons/spaceDetail/marker.svg';
+import OpacityPressable from '@/components/common/opacityPressable';
 import { SpaceDetailComponentProps } from '@/types/space';
 import { generateHtmlContent } from '@/utils/mapWebView';
 
-const SpaceLocationInfoComponent: React.FC<SpaceDetailComponentProps> = ({ data }) => {
+export default function SpaceLocationInfoComponent({ data }: SpaceDetailComponentProps) {
   const router = useRouter();
 
   const smallMapRef = useRef<WebView>(null);
 
   return (
-    <View className="gap-y-[16px]">
-      <View className="px-[20px] gap-y-[16px]">
-        <Text className="SUB2 text-black">위치 안내</Text>
+    <View>
+      <View className="gap-y-[16px]">
+        <View className="px-[20px] gap-y-[16px]">
+          <Text className="SUB2 text-black">위치 안내</Text>
 
-        <View className="flex flex-row justify-between items-center">
-          <View className="flex flex-row items-center gap-x-[2px]">
-            <MarkerIcon />
-            <Text numberOfLines={1} className="BODY1 text-dark_gray">
-              {data.city} {data.district} {data.address} {data.detailAddress} {data.detailAddress}
-            </Text>
+          <View className="flex flex-row justify-between items-center">
+            <View className="flex-1 flex flex-row items-center gap-x-[2px]">
+              <MarkerIcon />
+              <Text numberOfLines={1} className="flex-1 BODY1 text-dark_gray">
+                {data.city} {data.district} {data.address} {data.detailAddress}
+              </Text>
+            </View>
+
+            <OpacityPressable
+              onPress={async () => {
+                await Clipboard.setStringAsync(
+                  `${data.city} ${data.district} ${data.address} ${data.detailAddress}`,
+                );
+                Alert.alert('주소가 복사되었습니다.');
+              }}
+              className="px-[8px] shrink-0"
+            >
+              <Text className="CAP2 text-medium_gray underline">주소 복사</Text>
+            </OpacityPressable>
           </View>
 
           <Pressable
-            onPress={async () => {
-              await Clipboard.setStringAsync(
-                `${data.city} ${data.district} ${data.address} ${data.detailAddress} ${data.detailAddress}`,
-              );
-              Alert.alert('주소가 복사되었습니다.');
-            }}
-            className="px-[8px]"
+            onPress={() => router.push(`/space/${data.id}/map`)}
+            className="w-full h-[160px] rounded-xl"
           >
-            <Text className="CAP2 text-medium_gray underline">주소 복사</Text>
+            <WebView
+              ref={smallMapRef}
+              style={{ borderRadius: 12 }}
+              source={{ html: generateHtmlContent(data.latitude, data.longitude, false) }}
+              javaScriptEnabled={true}
+            />
           </Pressable>
         </View>
-
-        <Pressable
-          onPress={() => router.push(`/space/${data.id}/map`)}
-          className="w-full h-[160px] rounded-xl"
-        >
-          <WebView
-            ref={smallMapRef}
-            style={{ borderRadius: 12 }}
-            source={{ html: generateHtmlContent(data.latitude, data.longitude, false) }}
-            javaScriptEnabled={true}
-          />
-        </Pressable>
       </View>
+
+      <View className="h-[8px] bg-back_gray my-[24px]" />
     </View>
   );
-};
-
-export default SpaceLocationInfoComponent;
+}
