9de601a185f893d4988fe27c6218c805eb76af7f
JJHyeok13 <jjinh99@naver.com>
2025-09-21 15:58:24 +0900

    feat: 키보드를 포함하는 스크롤뷰 Provider 구현

---
+++ providers/keyBoardProvider.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+interface KeyBoardAwareProviderProps {
+  children: ReactNode;
+  isCenter?: boolean;
+  rowGap?: string | number;
+}
+
+export default function KeyBoardAwareProvider({
+  children,
+  isCenter = false,
+  rowGap,
+}: KeyBoardAwareProviderProps) {
+  return (
+    <KeyboardAwareScrollView
+      contentContainerStyle={{
+        flexGrow: 1,
+        justifyContent: isCenter ? 'center' : 'flex-start',
+        paddingHorizontal: 20,
+        rowGap: rowGap,
+        paddingBottom: 80,
+      }}
+      keyboardShouldPersistTaps="handled"
+      enableOnAndroid={true}
+      extraScrollHeight={20}
+    >
+      {children}
+    </KeyboardAwareScrollView>
+  );
+}
